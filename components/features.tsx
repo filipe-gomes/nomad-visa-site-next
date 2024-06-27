90b964fe030ae2f4d74b25fa50b9d880ee20302f
Filipe Gomes <filipegomes404@gmail.com>
2024-06-26 23:33:25 -0700

    first commit

---
+++ components/features.tsx
@@ -0,0 +1,163 @@
+export default function Features() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 mb-4">
+              At BW you can relax and rest assured that your relocation plans
+              will run as smoothly as possible.
+            </h2>
+            <p className="text-xl text-gray-400">
+              Welcome to your trusted partner in making your move to Brazil
+              seamless and stress-free. Specializing in visa assistance and
+              personalized relocation plans, we are dedicated to helping tech
+              nomads and other professionals transition smoothly to life in
+              Brazil.
+            </p>
+          </div>
+
+          {/* Items */}
+          <div
+            className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none"
+            data-aos-id-blocks
+          >
+            {/* 1st item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="w-16 h-16 mb-4"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  className="fill-current text-purple-600"
+                  width="64"
+                  height="64"
+                  rx="32"
+                />
+                <path
+                  className="stroke-current text-purple-100"
+                  d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                />
+                <path
+                  className="stroke-current text-purple-300"
+                  d="M43 42h-9M43 37h-9"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                />
+              </svg>
+              <h4 className="h4 mb-2">Due Diligence</h4>
+              <p className="text-lg text-gray-400 text-center">
+                We will help with review and preparation of necessary legal
+                documentation.
+              </p>
+            </div>
+
+            {/* 2nd item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-delay="100"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="w-16 h-16 mb-4"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  className="fill-current text-purple-600"
+                  cx="32"
+                  cy="32"
+                  r="32"
+                />
+                <path
+                  className="stroke-current text-purple-100"
+                  strokeWidth="2"
+                  strokeLinecap="square"
+                  d="M21 23h22v18H21z"
+                  fill="none"
+                  fillRule="evenodd"
+                />
+                <path
+                  className="stroke-current text-purple-300"
+                  d="M26 28h12M26 32h12M26 36h5"
+                  strokeWidth="2"
+                  strokeLinecap="square"
+                />
+              </svg>
+              <h4 className="h4 mb-2">Real Estate Investing</h4>
+              <p className="text-lg text-gray-400 text-center">
+                We will help with registration and documentation of real estate
+                purchased in Brazil, in addition to investment visa assistance.
+              </p>
+            </div>
+
+            {/* 3rd item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-delay="200"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="w-16 h-16 mb-4"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  className="fill-current text-purple-600"
+                  width="64"
+                  height="64"
+                  rx="32"
+                />
+                <g
+                  transform="translate(21 21)"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                >
+                  <ellipse
+                    className="stroke-current text-purple-300"
+                    cx="11"
+                    cy="11"
+                    rx="5.5"
+                    ry="11"
+                  />
+                  <path
+                    className="stroke-current text-purple-100"
+                    d="M11 0v22M0 11h22"
+                  />
+                  <circle
+                    className="stroke-current text-purple-100"
+                    cx="11"
+                    cy="11"
+                    r="11"
+                  />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">
+                Legal support and personalized consultancy
+              </h4>
+              <p className="text-lg text-gray-400 text-center">
+                We will offer complete legal support through the entire visa
+                process, along with personal consultancy sessions to determine
+                the best path to achieve legal residency.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ components/zigzag.tsx
@@ -0,0 +1,156 @@
+import Image from "next/image";
+
+import FeatImage01 from "@/public/images/features-03-image-01.png";
+import FeatImage02 from "@/public/images/features-03-image-02.png";
+import FeatImage03 from "@/public/images/features-03-image-03.png";
+
+export default function Zigzag() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">
+              Reach goals that matter
+            </div>
+            <h1 className="h2 mb-4">Why move to Brazil?</h1>
+            <p className="text-xl text-gray-400">
+              With a low cost of living, amazing food, growing tech sector and
+              diverse culture, it's no wonder people are flocking to Brazil.
+            </p>
+          </div>
+
+          {/* Items */}
+          <div className="grid gap-20">
+            {/* 1st item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage01}
+                  width={540}
+                  height={405}
+                  alt="Features 01"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right"
+              >
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <h3 className="h3 mb-3">Vibrant Culture</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    Brazil is known for its rich cultural heritage, including
+                    music, dance (such as samba and bossa nova), festivals like
+                    Carnival, and diverse cuisine.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* 2nd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage02}
+                  width={540}
+                  height={405}
+                  alt="Features 02"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-left"
+              >
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <h3 className="h3 mb-3">Beautiful Landscapes</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    From the Amazon Rainforest to the stunning beaches of Rio de
+                    Janeiro and the Iguazu Falls, Brazil offers breathtaking
+                    natural beauty.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* 3rd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage03}
+                  width={540}
+                  height={405}
+                  alt="Features 03"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right"
+              >
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <h3 className="h3 mb-3">Growing Economy</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    As one of the largest economies in South America, Brazil
+                    offers numerous opportunities for business and employment,
+                    especially in technology, agriculture, real estate and
+                    tourism sectors.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* 4th item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage02}
+                  width={540}
+                  height={405}
+                  alt="Features 02"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-left"
+              >
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <h3 className="h3 mb-3">Affordable Cost of Living</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    Compared to many Western countries, especially those in
+                    Europe and North America, the cost of living in Brazil can
+                    be much lower, making it an attractive destination for
+                    expatriates.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
